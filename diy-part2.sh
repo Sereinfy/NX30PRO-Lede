e7a863d6c5d890d201bfab23b4bcd10f9ba440b7
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2026-05-10 22:02:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,7 +31,3 @@ rm -rf package/feeds/packages/v2ray-geodata
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-argon/*
-cp -af package/luci-theme-argon/*  feeds/luci/themes/luci-theme-argon/
